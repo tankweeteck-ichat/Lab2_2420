66c57d77f9c75e4d0d27a1c22ee35d9116780e4f
kweeteck <tan_kwee_teck@ichat.sp.edu.sg>
2024-11-06 10:57:45 +0800

    Modifed bmi.py to return integer

--- bmi.py
+++ bmi.py
@@ -10,9 +10,17 @@ def calculate_bmi(height, weight):
     print("Weight Classification = ", end="")  # Use the end="" to suppress the newline
     if bmi<18.5:
         print("Under Weight")
+        return -1
     elif bmi>25.0:
         print("Over Weight")
+        return 1
     else:
         print("Normal Weight")
+        return 0
 
-calculate_bmi(weight=57, height=1.73)
+result = calculate_bmi(weight=37, height=1.73)
+print("Result = ", result)
+result = calculate_bmi(weight=57, height=1.73)
+print("Result = ", result)
+result = calculate_bmi(weight=97, height=1.73)
+print("Result = ", result)
